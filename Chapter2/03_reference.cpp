524937cdf98bfb2532956f97be71c5b4930715e0
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-01-29 20:58:37 +0530

    reference

---
+++ Chapter2/03_reference.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+
+float a =4.987;
+float &b =a;
+
+cout << "The value of a is " <<a << endl;
+cout << "The value of b is " <<b <<endl;
+
+
+
+
+
+return 0;
+
+}
