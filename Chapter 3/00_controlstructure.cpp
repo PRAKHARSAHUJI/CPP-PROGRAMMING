a924459946bd499b5aedfee12ac15889edcb26c7
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-01-30 01:38:31 +0530

    Controls Structure

---
+++ Chapter 3/00_controlstructure.cpp
@@ -0,0 +1,28 @@
+// Three Types of Basic Control Structure 
+
+//  1.Sequence Structure  
+//  2.Selection Structure 
+//  3.Loop Structure 
+
+// 1. Entry --> Action 1 --> Action 2 --> Exit 
+
+
+/* 2. Entry --> Condition 1 or Condition 2 --> If Condition 1 applies then Action 1 then Exit 
+    or If Condition 2 applies then Action 2 then Exit */
+
+
+/* 3. Entry --> Condition applies then Action 1 again then loop again iterates,  condition again action 1 again , 
+    if conditions fail then action 2 then after action 2 exit */
+
+
+// Ways to Model Control Structure 
+/* 1. IF ELSE 
+   2. If ELSE LADDER
+   3. SWITCH
+*/  
+
+//  1.Sequence Structure   These were programs like before in chapter 1 , chapter 3
+//  2.Selection Structure   IF ELSE, IF ELSE IF LADDER , SWITCH CASE
+
+
+
