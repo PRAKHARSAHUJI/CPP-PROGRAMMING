93096e47de8c3ea6c77fec0136daf6467773c05d
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-01-30 02:33:28 +0530

    Loops

--- Chapter 3/02_switch.cpp
+++ Chapter 3/02_switch.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
-using namespace std ;
+using namespace std;
 
 int main()
 {
-string name;
-int age;
-cout<< " Enter Your Name : ";
-cin >> name ;
-cout<< " Enter Your Age : ";
-cin >> age ;
+    string name;
+    int age;
+    cout << " Enter Your Name : ";
+    cin >> name;
+    cout << " Enter Your Age : ";
+    cin >> age;
 
-switch (age)
-{
+    switch (age)
+    {
     case 18:
-cout<< "You are 18";
-    break;
+        cout << "You are 18" << endl;
+        break;
     case 20:
-cout<< "You are 20";
-    break;
-default : 
-cout<<"Your age is not known";
-    break;
+        cout << "You are 20" << endl;
+        break;
+    default:
+        cout << "Your age is not known" << endl;
+        break;
+    }
+
+    return 0;
 }
 
-return 0;
-
-}
-
-
- /*  if break is not used it will print all the case below the given case */
+/*  if break is not used it will print all the case below the given case */
