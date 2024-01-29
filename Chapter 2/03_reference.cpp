83e84cfce8224134a4719e9c34636f51a01b827f
PRAKHARSAHUJI <prakharsahu@Prakhars-MacBook-Air.local>
2024-01-29 20:01:41 +0530

    Chapter 2

---
+++ Chapter 2/03_reference.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std ;
+float a = 3.14;
+
+int main()
+{
+int a = 22/7;
+
+cout << "The Value of a is " << a <<endl;
+cout << "The Value of Global a is " << :: a <<endl;     // :: this is scope resolution operator for global variables values take 
+
+
+
+
+return 0;
+
+}
---
+++ Chapter 2/05_typecasting.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int year = 2024;
+cout << 4+6;
+
+return 0;
+}
