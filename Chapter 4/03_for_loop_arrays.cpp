6164b874f28da1365673b0dab1249b461cebab9b
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-01-30 12:14:21 +0530

    For loop arrays

---
+++ Chapter 4/03_for_loop_arrays.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int englishmarks[] = {86,87,93,91};
+
+for(int i =0; i<4; i++)
+
+ cout<<"Marks Of English "<<i<<" is "<<englishmarks[i]<<endl;
+
+return 0;
+
+}
