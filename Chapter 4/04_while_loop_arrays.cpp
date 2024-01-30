b57858e20d9e6d33b964875fc2bbbedc1eea8bda
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-01-30 12:23:49 +0530

    While Loop Arrays

---
+++ Chapter 4/04_while_loop_arrays.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int mathsmarks[4];
+    mathsmarks[0]=99;
+    mathsmarks[1]=93;  
+    mathsmarks[2]=97;
+    mathsmarks[3]=95;
+
+int i=0;
+while(i<4)
+{
+ cout<<"Marks Of Maths "<<i<<" is "<<mathsmarks[i]<<endl;
+i++;
+}
+
+return 0;
+
+}
