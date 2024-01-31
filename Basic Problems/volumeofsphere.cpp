f2ec2dea2b2b718dfa9e030cf97d7edf948b22ca
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-01-31 21:22:59 +0530

    Volume of Sphere

--- Basic Problems/volumeofsphere.cpp
+++ Basic Problems/volumeofsphere.cpp
@@ -1,10 +1,20 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     double r;
+//     float volume = ((1.3333)* 3.1415);
+//     cin>>r;
+//     cout<<(volume*r*r*r);
+
+// }
 #include <iostream>
 using namespace std;
 int main()
 {
     double r;
-    float volume = ((1.3333)* 3.1415);
+    float volume = ((4.0/3)* 3.1415);
     cin>>r;
-    cout<<(volume*r*r*r);
+    cout<<(volume*r*r*r)<< endl;
 
 }
