81c80ae6cf21cc5f0c8f10fa004fe28d4c3733f5
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-01-31 23:05:02 +0530

    ASCII

--- Chapter 2/10_ASCI.cpp
+++ Chapter 2/10_ASCI.cpp
@@ -1,12 +1,49 @@
+//                             ---------------------- character to integer ---------------------------
+
+// #include <iostream>
+
+// using namespace std;
+// int main()
+// {
+//     char x;
+//     cin>>x;
+//     cout<<"ASCII value of  " <<x<<" is:  ";                             // character to integer
+//     cout<<(int)x<<endl;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     char x;
+//     cin>>x;
+//     int(y)=int(x) ;
+//     cout<<"ASCII value of  " <<x<<" is:  ";
+//     cout<<y<<endl;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     char x;
+//     cin>>x;
+//     cout<<"ASCII value of " <<x<<" is:  ";
+//     cout<<int(x)<<endl;
+// }
+
+//                             ----------------------  integer to character ---------------------------
+
 #include <iostream>
 using namespace std;
 int main()
 {
-    char x;
+    int x;
     cin>>x;
-
-    cout<<"ASCII value of  " <<x<<"is:  ";
-
-    cout<<(int)x<<endl;
-
+    char y=char(x);
+    cout<<"ASCII value of  " <<x<<" is :  ";
+    cout<<y<<endl;
+    return 0;
 }
