c5839b99a17802efe08a4e4ca4fc08931af60d7b
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-02-01 22:36:18 +0530

    Digits of a number

---
+++ Loops Problems/digits_of_number_using_while.cpp
@@ -0,0 +1,34 @@
+//                    ********************** USING FOR LOOP ********************************
+
+// #include <iostream>
+// using namespace std ;
+// int main()
+// {
+//     int n;
+//     cin>>n;
+// int count=0;
+//     for(;n>0;)
+//     {
+//         n=n/10;
+//         count++;
+//     }
+// cout<<count<<endl;
+// }
+
+
+//                    ********************** USING FOR LOOP ********************************
+
+#include <iostream>
+using namespace std ;
+int main()
+{
+    int n;
+    cin>>n;
+int count=0;
+    while(n!=0)
+    {
+        n=n/10;
+        count++;
+    }
+cout<<count<<endl;
+}
