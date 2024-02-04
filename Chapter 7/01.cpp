5c4ed8920bfb72912011c932d4b3ff8a70800a18
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-02-04 21:08:20 +0530

    Array

---
+++ Chapter 7/01.cpp
@@ -0,0 +1,43 @@
+//                             Declaration Method 1
+
+
+// # include <iostream>
+// using namespace std;
+// int main()
+// {
+// int arr[5];                // initialisation // declaration
+// arr[0] =9;
+// arr[1] =4;
+// arr[2] =7;
+// arr[3] =2;
+// arr[4] =4;
+
+// cout<< arr[0]<<endl;
+// cout<< arr[1]<<endl;
+// cout<< arr[2]<<endl;
+// cout<< arr[3]<<endl;
+// cout<< arr[4]<<endl;
+
+// }
+
+//                             Declaration Method 2
+
+
+# include <iostream>
+using namespace std;
+int main()
+{
+int arr[] ={1,3,4,5,6,7,8}   ;        // initialisation // declaration
+arr[0] =9;
+arr[1] =4;
+arr[2] =7;
+arr[3] =2;
+arr[4] =4;
+
+cout<< arr[0]<<endl;
+cout<< arr[1]<<endl;
+cout<< arr[2]<<endl;
+cout<< arr[3]<<endl;
+cout<< arr[4]<<endl;
+
+}
