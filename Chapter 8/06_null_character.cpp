9d97e2b3c0c622bcb0229b36b17295ece62f28aa
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-02-05 22:53:48 +0530

    Strings

---
+++ Chapter 8/06_null_character.cpp
@@ -0,0 +1,22 @@
+// # include <iostream>
+// using namespace std;
+// int main()
+// {
+
+// int a=0; 
+// cout<<char(a)<<endl;
+
+// }
+
+
+# include <iostream>
+using namespace std;
+int main()
+{
+
+char a='\0'; 
+cout<<int(a)<<endl;
+
+}
+
+//                --------->    ' \0 ' this denotes where the string is ending .
