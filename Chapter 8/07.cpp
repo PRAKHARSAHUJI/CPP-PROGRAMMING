3444f81e637b3607c6d4b6f2f4ca45d3d4b8c371
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-02-05 22:52:13 +0530

    Strings

---
+++ Chapter 8/07.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+
+string s ="My Name Is Prakhar Sahu ";
+cout<<s<<endl;
+
+int n = s.length();
+
+for (int i=0;i<n;i++)
+{
+    cout<<s[i];
+}
+
+cout<<endl;
+}
