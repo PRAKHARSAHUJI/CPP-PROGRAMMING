390a86c8e43b921065f452800042842165b74de0
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-02-05 19:18:16 +0530

    Strings basic

---
+++ Chapter 8/01.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+
+string s ="My Name Is Prakhar Sahu ";
+cout<<s<<endl;
+
+}
---
+++ Chapter 8/02.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+
+string s ;
+cin>>s;                   //  Prakhar Sahu 
+cout<<s<<endl;            //  If you input string it will before the space one only 
+
+}
+
+//                        string s ="My Name Is Prakhar Sahu ";
+//                        cout<<s<<endl;
+//                       // here in this it will print complete as it is 
+      
